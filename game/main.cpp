7bdc36c934b5c50ae0c335cec831cdc935a4d0b8
20161104613 <a20161104613@mac01.local>
2018-06-25 10:49:52 +0800

    Initial Commit

---
+++ game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  game
+//
+//  Created by 20161104613 on 2018/6/25.
+//  Copyright © 2018年 20161104613. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
